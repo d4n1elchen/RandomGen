f37dee84d4e2fcff92dd9feae89253aaf1a07b33
Daniel Chen <team6612@gmail.com>
2016-04-22 21:47:38 +0800

    first commit

---
+++ random.js
@@ -0,0 +1,24 @@
+function shuffle(arr) {
+  var i = arr.length;
+  var j = 0;
+  var temp;
+
+  while(i--) {
+    j = Math.floor( Math.random() * (i+1) );
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  return arr;
+}
+
+var arr_s = prompt("Enter a list seperated by space, ex: 'Daniel DC Chen'");
+var arr = arr_s.split(" ");
+var arr = shuffle(arr);
+var str = "";
+for (var a in arr) {
+  var i = 1 + parseInt(a);
+  str += i + ": " + arr[a] + "\n";
+}
+alert(str);
